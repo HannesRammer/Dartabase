3ed1e17265d21b2d18d439a5c6d3868418dc7189
Hannes Rammer <Hannes.Rammer@gmail.com>
2013-11-14 00:23:57 +0100

    added core

---
+++ dartabase_core/README.md
@@ -0,0 +1,11 @@
+Dartabase Core 0.1.0
+===================
+
+    used by Dartabase Tools
+    
+    Dartabase Migration 
+		
+		Serverside Database migration for simple version controlled database structure manipulation 
+    	inspired by Ruby on Rails migrations 
+    
+    Dartabase Models
---
+++ dartabase_core/lib/dartabase_core.dart
@@ -0,0 +1,91 @@
+library dartabaseCore;
+
+import "dart:io";
+import "dart:convert";
+
+
+
+
+
+
+class DBCore {
+  static String rootPath;
+  static Map parsedMap ;
+
+  static Map parsedMapping ;
+
+  static String adapter;
+  static String get MySQL => "MySQL";
+
+  static String get PGSQL => "PGSQL";
+
+  static String database;
+
+  static String username;
+
+  static String password;
+
+  static String host;
+
+  static int port;
+
+  static String schemaVersion;
+  
+   static Map jsonFilePathToMap(String path) {
+    var file = new File(path);
+    String fileText = file.readAsStringSync(encoding: ASCII);
+    if (fileText == "") {
+      fileText = "{}";
+    }
+    return  JSON.decode(fileText);
+  }
+  
+   static Map loadSchemaToMap() {
+    var schema = new File('$rootPath/db/schema.json');
+    schema.writeAsStringSync("", encoding: ASCII, mode: FileMode.APPEND);
+    String fileText = schema.readAsStringSync(encoding: ASCII);
+    if (fileText == "") {
+      fileText = "{}";
+    }
+    return  JSON.decode(fileText);
+  }
+   static String primaryIDColumnString(String adapter) {
+     String sql;
+     if(adapter == DBCore.MySQL){
+       sql = "id ${DBCore.typeMapping("INT")} NOT NULL AUTO_INCREMENT PRIMARY KEY,";
+     }else if(adapter == DBCore.PGSQL){
+       sql = "id ${DBCore.typeMapping("INT")} NOT NULL PRIMARY KEY,";
+     }
+     
+     return sql;
+  }
+  
+   static void mapToJsonFilePath(Map contentMap,String filePath) {
+    String contentString = JSON.encode(contentMap);
+    var file = new File(filePath);
+    
+    file.writeAsStringSync(contentString, encoding: ASCII);
+    
+  }
+  
+   static String typeMapping(String dartabaseType) {
+    return parsedMapping[dartabaseType];
+  }
+   
+   static void loadConfigFile() {
+//parsedMap = DBHelper.jsonFilePathToMap('$rootPath/db/configPGSQL.json');
+//parsedMap = DBHelper.jsonFilePathToMap('$rootPath/db/configMYSQL.json');
+     parsedMap = jsonFilePathToMap('$rootPath/db/config.json');
+     adapter = parsedMap["adapter"];
+     if (adapter != MySQL && adapter != PGSQL) {
+       print("\nadapter in config file not correct!!! Should be '$MySQL' or '$PGSQL'!!!");
+     }
+     database = parsedMap["database"];
+     username = parsedMap["username"];
+     password = parsedMap["password"];
+     host = parsedMap["host"];
+     port = int.parse(parsedMap["port"]);
+     
+     schemaVersion = DBCore.jsonFilePathToMap('$rootPath/db/schemaVersion.json')["schemaVersion"];
+   }
+}
