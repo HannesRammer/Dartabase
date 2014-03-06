54ab759a7dc8522db6cd0f8fe9f62eb8f71ec14b
Hannes Rammer <Hannes.Rammer@gmail.com>
2014-03-06 18:45:46 +0100

    moved examples

--- examples/PreTutorialMixApp/web/index.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:html';
-
-void main() {
-  querySelector("#view_list").onClick.listen((e) => window.location.assign("item/index.html"));
-  querySelector("#view_inline_list").onClick.listen((e) => window.location.assign("item/index.html?inlineEdit=true"));
-}
-
-
