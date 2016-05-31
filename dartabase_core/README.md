ef3eb21d4c5fe5a2afa4fc7fc2bed1a09a64880a
Hannes Rammer <Hannes.Rammer@gmail.com>
2016-05-31 19:46:49 +0200

    now using prettyprint when storing JSON text to files

--- dartabase_core/README.md
+++ dartabase_core/README.md
@@ -3,6 +3,8 @@ Dartabase Core 0.4.0
 
     shared lib used by Dartabase Tools (no need for you to do anything)
     
+    changelog 0.4.1 
+    now using pretty print to store json in files
     changelog 0.4.0 
     now working with _InternalLinkedHashMap instead of _LinkedHashMap
     
--- dartabase_core/lib/dartabase_core.dart
+++ dartabase_core/lib/dartabase_core.dart
@@ -5,100 +5,102 @@ import "dart:convert";
 import "package:dev_string_converter/dev_string_converter.dart" as DSC;
 
 class DBCore {
-  static String rootPath;
-  static Map parsedMap;
+    static String rootPath;
+    static Map parsedMap;
 
-  static Map parsedMapping;
+    static Map parsedMapping;
 
-  static String adapter;
+    static String adapter;
 
-  static String get MySQL => "MySQL";
+    static String get MySQL => "MySQL";
 
-  static String get PGSQL => "PGSQL";
+    static String get PGSQL => "PGSQL";
 
-  static String database;
+    static String database;
 
-  static String username;
+    static String username;
 
-  static String password;
+    static String password;
 
-  static String host;
+    static String host;
 
-  static int port;
+    static int port;
 
-  static bool ssl;
+    static bool ssl;
 
-  static String schemaVersion;
+    static String schemaVersion;
 
-  static Map jsonFilePathToMap(String path) {
-
+    static Map jsonFilePathToMap(String path) {
         File file = new File(path);
-    if (file.existsSync()) {
-      String fileText = file.readAsStringSync(encoding: ASCII);
-      if (fileText == "") {
-        fileText = "{}";
-      }
-      return JSON.decode(fileText);
-    } else {
-      return {};
+        if (file.existsSync()) {
+            String fileText = file.readAsStringSync(encoding: ASCII);
+            if (fileText == "") {
+                fileText = "{}";
+            }
+            return JSON.decode(fileText);
+        } else {
+            return {};
+        }
     }
-  }
 
-  static Map loadSchemaToMap(String rootPath) {
-    var schema = new File('$rootPath/db/schema.json');
-    schema.writeAsStringSync("", encoding: ASCII, mode: FileMode.APPEND);
-    String fileText = schema.readAsStringSync(encoding: ASCII);
-    if (fileText == "") {
-      fileText = "{}";
+    static Map loadSchemaToMap(String rootPath) {
+        var schema = new File('$rootPath/db/schema.json');
+        schema.writeAsStringSync("", encoding: ASCII, mode: FileMode.APPEND);
+        String fileText = schema.readAsStringSync(encoding: ASCII);
+        if (fileText == "") {
+            fileText = "{}";
+        }
+        return JSON.decode(fileText);
     }
-    return JSON.decode(fileText);
-  }
 
-  static void mapToJsonFilePath(Map contentMap, String filePath) {
-    String contentString = JSON.encode(contentMap);
-    var file = new File(filePath);
+    static void mapToJsonFilePath(Map contentMap, String filePath) {
+        JsonEncoder encoder = new JsonEncoder.withIndent('  ');
+        String ppContentString = encoder.convert(contentMap);
 
-    file.writeAsStringSync(contentString, encoding: ASCII);
+        print(ppContentString);
 
-  }
+        String contentString = JSON.encode(contentMap);
+        var file = new File(filePath);
 
-  static void stringToFilePath(String text, String filePath) {
-    var file = new File(filePath);
+        file.writeAsStringSync(ppContentString, encoding: ASCII);
+    }
 
-    file.writeAsStringSync(text, encoding: ASCII);
+    static void stringToFilePath(String text, String filePath) {
+        var file = new File(filePath);
 
-  }
+        file.writeAsStringSync(text, encoding: ASCII);
+    }
 
-  static String typeMapping(String dartabaseType) {
-    return parsedMapping[dartabaseType];
-  }
+    static String typeMapping(String dartabaseType) {
+        return parsedMapping[dartabaseType];
+    }
 
-  static defaultValueFor(var mapOrString) {
-    String dartabaseType;
-    if (mapOrString.runtimeType == String) {
-      return defaultForType(mapOrString);
+    static defaultValueFor(var mapOrString) {
+        String dartabaseType;
+        if (mapOrString.runtimeType == String) {
+            return defaultForType(mapOrString);
         } else if (mapOrString.runtimeType.toString() == "_InternalLinkedHashMap") {
-      dartabaseType = mapOrString["type"];
-      String defaultValue = mapOrString["default"];
-      if (defaultValue == null) {
-        return defaultForType(dartabaseType);
-      } else {
-        return defaultValue;
-      }
+            dartabaseType = mapOrString["type"];
+            String defaultValue = mapOrString["default"];
+            if (defaultValue == null) {
+                return defaultForType(dartabaseType);
+            } else {
+                return defaultValue;
+            }
+        }
     }
-  }
 
-  static dbType(var mapOrString) {
-    String dartabaseType;
-    if (mapOrString.runtimeType == String) {
-      return mapOrString;
+    static dbType(var mapOrString) {
+        String dartabaseType;
+        if (mapOrString.runtimeType == String) {
+            return mapOrString;
         } else if (mapOrString.runtimeType.toString() == "_InternalLinkedHashMap") {
-      dartabaseType = mapOrString["type"];
-      return dartabaseType;
+            dartabaseType = mapOrString["type"];
+            return dartabaseType;
+        }
     }
-  }
 
-  static defaultForType(dartabaseType) {
+    static defaultForType(dartabaseType) {
         if ([
             "BINT",
             "BINT UNSIGNED",
@@ -112,20 +114,20 @@ class DBCore {
             "TINT",
             "TINT UNSIGNED"
         ].contains(dartabaseType)) {
-      return 0;
+            return 0;
         } else if (["DOUBLE", "FLOAT", "FLOAT UNSIGNED"].contains(
                 dartabaseType)) {
-      return 0.0;
-    } else if (dartabaseType == "BOOLEAN") {
-      return false;
+            return 0.0;
+        } else if (dartabaseType == "BOOLEAN") {
+            return false;
         } else if (["CHAR", "LTEXT", "MTEXT", "TEXT", "TTEXT", "VARCHAR"]
                 .contains(dartabaseType)) {
-      return "";
+            return "";
         } else if (["DATE", "DATETIME", "TIME", "TIMESTAMP"].contains(
                 dartabaseType)) {
-      return new DateTime.now();
-    } else {
-      /*
+            return new DateTime.now();
+        } else {
+            /*
        "BINARY": "bytea",
        "BIT": "bytea",
        "BLOB": "bytea",
@@ -134,10 +136,10 @@ class DBCore {
        "MBLOB": "bytea",
        "TBLOB": "bytea",
        "VARBINARY": "bytea",*/
+        }
     }
-  }
 
-  static dartabaseTypeToDartType(dartabaseType) {
+    static dartabaseTypeToDartType(dartabaseType) {
         if ([
             "BINT",
             "BINT UNSIGNED",
@@ -151,21 +153,21 @@ class DBCore {
             "TINT",
             "TINT UNSIGNED"
         ].contains(dartabaseType)) {
-      return "num";
+            return "num";
         } else if (["DOUBLE", "FLOAT", "FLOAT UNSIGNED"].contains(
                 dartabaseType)) {
-      return "double";
-    } else if (dartabaseType == "BOOLEAN") {
-      return "bool";
+            return "double";
+        } else if (dartabaseType == "BOOLEAN") {
+            return "bool";
         } else if (["CHAR", "LTEXT", "MTEXT", "TEXT", "TTEXT", "VARCHAR"]
                 .contains(dartabaseType)) {
-      return "String";
+            return "String";
         } else if (["DATE", "DATETIME", "TIME", "TIMESTAMP"].contains(
                 dartabaseType)) {
-      return "DateTime";
-    } else {
-      return "List";
-      /*
+            return "DateTime";
+        } else {
+            return "List";
+            /*
         "BINARY": "bytea",
         "BIT": "bytea",
         "BLOB": "bytea",
@@ -174,44 +176,44 @@ class DBCore {
         "MBLOB": "bytea",
         "TBLOB": "bytea",
         "VARBINARY": "bytea",*/
-    }
-  }
-
-  static void loadConfigFile(String rootPath) {
-    //parsedMap = DBHelper.jsonFilePathToMap('$rootPath/db/configPGSQL.json');
-    //parsedMap = DBHelper.jsonFilePathToMap('$rootPath/db/configMYSQL.json');
-    parsedMap = jsonFilePathToMap('$rootPath/db/config.json');
-    adapter = parsedMap["adapter"];
-    if (adapter != MySQL && adapter != PGSQL) {
-      print("\nadapter in config file not correct!!! Should be '$MySQL' or '$PGSQL'!!!");
-    }
-    database = parsedMap["database"];
-    username = parsedMap["username"];
-    password = parsedMap["password"];
-    host = parsedMap["host"];
-    port = int.parse(parsedMap["port"]);
-    if (parsedMap["ssl"] == "true") {
-      ssl = true;
-    } else {
-      ssl = false;
+        }
     }
 
-    schemaVersion = DBCore.jsonFilePathToMap('$rootPath/db/schemaVersion.json')["schemaVersion"];
-  }
+    static void loadConfigFile(String rootPath) {
+        //parsedMap = DBHelper.jsonFilePathToMap('$rootPath/db/configPGSQL.json');
+        //parsedMap = DBHelper.jsonFilePathToMap('$rootPath/db/configMYSQL.json');
+        parsedMap = jsonFilePathToMap('$rootPath/db/config.json');
+        adapter = parsedMap["adapter"];
+        if (adapter != MySQL && adapter != PGSQL) {
+            print("\nadapter in config file not correct!!! Should be '$MySQL' or '$PGSQL'!!!");
+        }
+        database = parsedMap["database"];
+        username = parsedMap["username"];
+        password = parsedMap["password"];
+        host = parsedMap["host"];
+        port = int.parse(parsedMap["port"]);
+        if (parsedMap["ssl"] == "true") {
+            ssl = true;
+        } else {
+            ssl = false;
+        }
 
-  static String toTableName(String text) {
-    return DSC.toTableName(text);
-  }
+        schemaVersion = DBCore.jsonFilePathToMap('$rootPath/db/schemaVersion.json')["schemaVersion"];
+    }
 
-  static String toPolyName(String text) {
-    return DSC.toPolyName(text);
-  }
+    static String toTableName(String text) {
+        return DSC.toTableName(text);
+    }
 
-  static String toVarName(String text) {
-    return DSC.toVarName(text);
-  }
+    static String toPolyName(String text) {
+        return DSC.toPolyName(text);
+    }
 
-  static String toClassName(String text) {
-    return DSC.toClassName(text);
-  }
+    static String toVarName(String text) {
+        return DSC.toVarName(text);
+    }
+
+    static String toClassName(String text) {
+        return DSC.toClassName(text);
+    }
 }
